9a48a10e1959c1264087172d730dc00e975e200f
Andrew Shlegel <a.shlegel@outlook.com>
2023-02-23 10:10:49 -0300

    Example005_If_else added

---
+++ Example005_If_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! это маша!");
+}
+else
+{
+    Console.WriteLine("Привет, " + username);
+}
