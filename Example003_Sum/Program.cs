70500e906f56ff084554fcd5443c97381f483a75
Andrew Shlegel <a.shlegel@outlook.com>
2023-02-23 09:55:28 -0300

    Example004_Div added

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -2,4 +2,6 @@
 int numberB = new Random().Next(1, 10);
 int result = numberA + numberB;
 
-Console.WriteLine(result);
+Console.WriteLine("Первое число: " + numberA);
+Console.WriteLine("Второе число: " + numberB);
+Console.WriteLine("Сумма: " + result);
---
+++ Example004_Div/Program.cs
@@ -0,0 +1,7 @@
+﻿double numberA = new Random().Next(1, 30);
+double numberB = new Random().Next(1, 30);
+double result = numberA / numberB;
+
+Console.WriteLine("Первое число: " + numberA);
+Console.WriteLine("Второе число: " + numberB);
+Console.WriteLine("Результат деления: " + result);
