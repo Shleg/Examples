2bc8c34e161265355a92602f43858819d7f2a5f8
Andrew Shlegel <a.shlegel@outlook.com>
2023-02-23 10:56:53 -0300

    Example008_Dog added

---
+++ Example008_Dog/Program.cs
@@ -0,0 +1,31 @@
+﻿int count = 0;
+int meetPoint = 10;
+int friendFlag = 2;
+double time = 0;
+
+Console.Write("Введите скорость первого друга: ");
+int firstFriendSpeed = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите скорость второго друга: ");
+int secondFriendSpeed = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите скорость собаки: ");
+int dogSpeed = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите дистанцию: ");
+double distance = Convert.ToInt32(Console.ReadLine());
+
+while (distance > meetPoint)
+{
+    if (friendFlag == 1)
+    {
+        time = distance / (dogSpeed + firstFriendSpeed);
+        friendFlag = 2;
+    }
+    else
+    {
+        time = distance / (dogSpeed - secondFriendSpeed);
+        friendFlag = 1;
+    }
+    distance = distance - time * (firstFriendSpeed - secondFriendSpeed);
+    count++;
+}
+
+Console.WriteLine("Собака пробежит " + count + " раз");
